f486ca25d2a1b6ad1961e37c34d8cddc46135713
Pratik Dey <pratikdey.pro@gmail.com>
2025-11-02 19:35:50 +0530

    Added intial code and tcs

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -3,7 +3,7 @@ package org.example;
 public class StringCalculator {
     public int add(String numbers) {
         if(numbers == null || numbers.isEmpty()) {
-            return 2;
+            return 1;
         }
         return Integer.parseInt(numbers);
     }
