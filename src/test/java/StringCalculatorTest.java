1eec4fd8d60ca54b1be6cd39991bd718eb67d107
pratikdey <pratikdey.pro@gmail.com>
2025-11-02 18:48:28 +0530

    Added first test and code

---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+import org.example.StringCalculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringCalculatorTest {
+    @Test
+    public void emptyStringReturnsZero() {
+        StringCalculator calc = new StringCalculator();
+        assertEquals(0, calc.add(""));
+    }
+}
