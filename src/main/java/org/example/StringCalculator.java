c3e31de04a3a29715cecfcc939c7984802783c26
pratikdey <pratikdey.pro@gmail.com>
2025-11-02 19:21:21 +0530

    Added first test and code

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -3,7 +3,7 @@ package org.example;
 public class StringCalculator {
     public int add(String numbers) {
         if(numbers == null || numbers.isEmpty()) {
-            return 0;
+            return 2;
         }
         return Integer.parseInt(numbers);
     }
